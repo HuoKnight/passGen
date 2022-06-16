be21c6f0e3dbe94a372cd2184bbacabbf47ea735
HuoKnight <noman.gronckle@gmail.com>
2022-06-16 12:56:22 -0700

    commit whatever is here to wipe machine

--- passGen.jl
+++ passGen.jl
@@ -1,18 +1,36 @@
 using Random
 
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '?', '!', '@', '#', '$', '%', '&', '(', ')', '{', '}', '[', ']']
+words = String(read("words.dat")) |> Meta.parse |> eval
+passLength = parse(Int64, ARGS[1])
+
 
 passArr = []
 
 
-for i in 1:parse(Int64, ARGS[1])
-    char = rand(1:39)
+for i in 1:passLength
+    char = rand(1:50)
 
+    
     if char <= 26 && rand((1, 2)) == 2
         push!(passArr, uppercase(chars[char]))
+    elseif char >= 40
+        for b in 1:length(words)
+            word = words[rand(1:2315)]
+            if passLength - i > length(word)
+                push!(passArr, word)
+                break
+            else
+                push!(passArr, chars[rand(27:39)])
+                break
+            end
+        end
     else
-        push!(passArr, chars[rand(1:39)])
+        push!(passArr, chars[char])
+    end
+    if length(join(passArr)) == passLength
+        break
     end
 end
 
-println("Password: ", join(passArr))
+println("Password: ", join(passArr), "\n", "Length: ", length(join(passArr)))
